b108e37784e0bbd70e11b2f64ba26b199745db01
Jonathan E. Emmett <jon@emmettmedia.com>
2022-10-31 18:23:17 -0500

    added some error handling on login

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -4,6 +4,47 @@
     export let data;
     export let form;
 
+    // Variables
+    let email = '';
+    let password = '';
+
+    // Errors
+    let emailError = false;
+    let passwordError = false;
+
+    // Functions
+    function handleEmailChange(e) {
+        if(e.target.value.length > 0 && e.target.value.includes('@')) {
+            emailError = false;
+        }
+    }
+
+    function handlePasswordChange(e) {
+        if(e.target.value.length > 0) {
+            passwordError = false;
+        }
+    }
+
+    function validate() {
+        let isValid = true;
+
+        if(email === '' || !email.includes('@')) {
+            emailError = true;
+            isValid = false;
+        } else {
+            emailError = false;
+        }
+
+        if(password === '') {
+            passwordError = true;
+            isValid = false;
+        } else {
+            passwordError = false;
+        }
+
+        return isValid;
+    }
+
     
 </script>
 
@@ -13,7 +54,11 @@
             <h2>Login</h2>
         </div>
         <div class="card-content">
-            <form method="POST" action='?/login' use:enhance={()=> {
+            <form method="POST" action='?/login' use:enhance={({ cancel })=> {
+                if(!validate()){
+                    cancel();
+                }
+
                 return async ({ result }) => {
                     invalidateAll();
 
@@ -21,12 +66,16 @@
                 }
             }}>
                 <div class="form-group">
-                    <label for="email" class="form-control-label">Email</label>
-                    <input type="email" class="form-control" name="email" placeholder="Email">
+                    <input type="email" class="form-control" name="email" placeholder="Email" bind:value={email} on:blur={(e) => handleEmailChange(e)}>
+                    {#if emailError}
+                        <div class="error">Please enter a valid email address</div>
+                    {/if}
                 </div>
                 <div class="form-group">
-                    <label for="password" class="form-control-label">Password</label>
-                    <input type="password" class="form-control" name="password" placeholder="Password">
+                    <input type="password" class="form-control" name="password" placeholder="Password" bind:value={password} on:blur={(e) => handlePasswordChange(e)}>
+                    {#if passwordError}
+                        <div class="error">Please enter a password</div>
+                    {/if}
                 </div>
                 {#if form?.invalid}
                     <div class="error">Invalid username or password.</div>
--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,5 +1,4 @@
 export const load = async ({ locals }) => {
-	console.log('locals', locals);
 	return {
 		user: locals?.user
 	};
